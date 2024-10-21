21568611bf4adfb25a4bc8d772d21617e221f666
Syed Nayab Bukhari <bec_dir@yahoo.com>
2024-10-21 13:34:32 +1100

    inital
    moodle-mod_flipbook project conversion

---
+++ flipbook/extras/flipbook.js
@@ -0,0 +1,23 @@
+var flipbookcfg={
+    url: ".\/data\/",
+    cover: 0,
+    numPages: 6,
+    textTip: "Press ESC to exit. Use the keyboard arrows to change the page.",
+    background: ".\/flipbook\/files\/backgrounds\/back13.svg",
+    showSlider: "null",
+    showSocial: "null",
+    showformat: "1",
+    companyLogo: "https:\/\/dev.lms.net.au\/pluginfile.php\/1\/theme_elan\/main_logo\/1729296333\/100x392-white-elearningMarketplace.PNG",
+    title: "Syed Nayab Bukhari CV",
+    tagtitle: "Syed Nayab Bukhari CV", 
+   description: "Syed Nayab Bukhari CV", 
+    showDownload: "null",
+    backlink: "https:\/\/lms.net.au",
+    opacity: "0.4",
+    name: "syed.pdf",
+    direction: "ltr", 
+    flipsound: "null", 
+    widthimg: "1488",
+    heightimg: "2105" 
+    
+};
